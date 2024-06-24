70d493104905ba776d981a753268c05aa552ef7c
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2024-06-24 14:12:58 -0400

    reviewing types for TS and Golang (#1)
    * reviewing types for TS and Golang
    
    * fixing codeowners

--- typescript/types.ts
+++ typescript/types.ts
@@ -1,74 +1,89 @@
-interface CoprocessorRequest {
-  version: number;
-  stage: RouterStage;
-  control: 'continue';
-  id: string;
-  headers?: Record<string, string[]>;
-  body?: string | object;
-  context?: RouterContext;
-  sdl?: string;
-  path?: string;
-  method?: string;
-  serviceName?: string;
-  statusCode?: number;
-  uri?: string;
-  hasNext?: boolean;
+// Type definitions pulled from the following link: https://www.apollographql.com/docs/router/customizations/coprocessor#property-reference
+interface Coprocessor {
+    id: string;
+    version: number;
+    stage: RouterStage;
+    headers?: Record<string, string[]>;
+    body?: string | object;
+    context?: RouterContext;
+    sdl?: string;
+    path?: string;
+    method?: string;
+    serviceName?: string;
+    statusCode?: number;
+    uri?: string;
+    hasNext?: boolean;
+    query_plan?: object; // https://www.apollographql.com/docs/router/customizations/coprocessor#query_plan
 }
 
-interface CoprocessorResponse extends CoprocessorRequest {
-  control: CoprocessorControl;
+interface CoprocessorRequest extends Coprocessor {
+    control: 'continue';
+}
+
+interface CoprocessorControlBreak {
+    break: number;
+}
+
+type CoprocessorControl = 'continue' | CoprocessorControlBreak;
+
+interface CoprocessorResponse extends Coprocessor {
+    control: CoprocessorControl;
 }
 
 enum RouterStage {
-  RouterRequest = 'RouterRequest',
-  RouterResponse = 'RouterResponse',
-  SupergraphRequest = 'SupergraphRequest',
-  SupergraphResponse = 'SupergraphResponse',
-  SubgraphRequest = 'SubgraphRequest',
-  SubgraphResponse = 'SubgraphResponse'
+    RouterRequest = 'RouterRequest',
+    RouterResponse = 'RouterResponse',
+    SupergraphRequest = 'SupergraphRequest',
+    SupergraphResponse = 'SupergraphResponse',
+    ExecutionRequest = 'ExecutionRequest',
+    ExecutionResponse = 'ExecutionResponse',
+    SubgraphRequest = 'SubgraphRequest',
+    SubgraphResponse = 'SubgraphResponse'
 }
 
 type RouterControl = 'continue' | RouterControlBreak;
 
 interface RouterControlBreak {
-  break: number;
+    break: number;
 }
 
 interface RouterContext {
-  entries: Record<string, any>;
+    entries: Record<string, any>;
 }
 
 // Example usage:
 const exampleRequest: CoprocessorRequest = {
-  version: 1,
-  stage: 'RouterRequest',
-  control: 'continue',
-  id: '1b19c05fdafc521016df33148ad63c1b',
-  headers: {
-    "content-type": ["application/json"]
-  },
-  body: {
-    query: "query GetActiveUser { me { name } }"
-  },
-  context: {
-    entries: {
-      "accepts-json": true
-    }
-  },
-  path: "/",
-  method: "POST"
+    version: 1,
+    stage: RouterStage.RouterRequest,
+    control: 'continue',
+    id: '1b19c05fdafc521016df33148ad63c1b',
+    headers: {
+        "content-type": ["application/json"]
+    },
+    body: {
+        query: "query GetActiveUser { me { name } }"
+    },
+    context: {
+        entries: {
+            "accepts-json": true
+        }
+    },
+    path: "/",
+    method: "POST"
 };
 
+// exampleRequest will be used here to fill out the necessary values to send back to the router
 const exampleResponse: CoprocessorResponse = {
-  control: { break: 401 },
-  body: {
-    errors: [
-      {
-        message: "Not authenticated.",
-        extensions: {
-          code: "ERR_UNAUTHENTICATED"
-        }
-      }
-    ]
-  }
+    ...exampleRequest,
+    control: { break: 401 },
+    body: {
+        errors: [
+            {
+                message: "Not authenticated.",
+                extensions: {
+                    code: "ERR_UNAUTHENTICATED"
+                }
+            }
+        ]
+    }
 };
